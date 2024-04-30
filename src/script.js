f4490dd42b56464e223fe6a202334f568de9c521
Diego <diego@diegog.io>
2024-04-29 23:45:30 -0600

    Fix script

--- src/script.js
+++ src/script.js
@@ -1,30 +1,44 @@
 (async function () {
-    const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    let date = new Date(new Date().toLocaleDateString("en-US", { timeZone: tz })).toISOString().split('T')[0];;
-    const response = await fetch(`https://www.balldontlie.io/api/v1/games?dates[]=${date}&team_ids[]=16`);
-    const games = await response.json()
-    let game = false;
-    if (games.data[0]){
-        game = games.data[0]
-    }
-    let display = {};
-    if (game) {
-        statement = game.home_team.city === 'Miami'
-            ? `and it's a fucking home game, against the ${game.visitor_team.name}`
-            : `it's in ${game.home_team.city} against the fucking ${game.home_team.name}`
-  
-      display = {
-        game: 'YES',
-        statement
-      }
-    }
-    else {
-      display = {
-        game: 'NO',
-        statement: 'there is no fucking game today'
-      }
-    }
-    
-    document.getElementById('game').innerHTML = display.game;
-    document.getElementById('statement').innerHTML = display.statement;
+    await getGameData().then((data) => displayInformation(data));
 })();
+
+async function getGameData() {
+    const response = await fetch(`https://api.diegog.io/game-today`);
+    let { game } = await response.json();
+    
+    if (!game) {
+      return false;
+    }
+
+    const homeGame = game.homeTeam.teamTricode === 'MIA';
+
+    return {
+      homeGame,
+      city: game.homeTeam.teamCity,
+      opponent: homeGame
+        ? game.awayTeam.teamName 
+        : game.homeTeam.teamName
+    }
+}
+
+function displayInformation(gameData) {
+  const gameElement = document.getElementById('game');
+  const statementElement = document.getElementById('statement');
+
+  const { statement, gameToday } = gameData
+    ? 
+      {
+        statement: gameData.homeGame
+          ? `and it's a fucking home game, against the ${gameData.opponent}`
+          : `it's in ${gameData.city} against the fucking ${gameData.opponent}`,
+        gameToday: 'YES'
+      }
+    : 
+      {
+        statement: 'there is no fucking game today',
+        gameToday: 'NO'
+      }
+
+    gameElement.innerHTML = gameToday;
+    statementElement.innerHTML = statement;
+}
